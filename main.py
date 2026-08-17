21a64be2df006851993ab6bb5001ff3dba869f9b
Mahdie Mirjalili <mwhdmir82@gmail.com>
2026-08-17 16:04:49 +0330

    feat: add account creation and main menu

--- main.py
+++ main.py
@@ -1,12 +1,48 @@
-from banking.models import SavingsAccount
+from banking.models import BankAccount, SavingsAccount
 
-account = SavingsAccount("2001", "Ali", 0.05, 500)
+def create_account():
+    print("1. Standard Account")
+    print("2. Savings Account")
 
-account.deposit(1000)
+    account_type = input("Choose account type: ")
 
-print("Before interest:", account.balance)
+    if account_type == "1":
+        account_number = input("Enter account number: ")
+        owner_name = input("Enter owner name: ")
 
-account.apply_interest()
+        return BankAccount(account_number, owner_name)
 
-print("After interest:", account.balance)
-print(account.get_transaction_history())
+    if account_type == "2":
+        account_number = input("Enter account number: ")
+        owner_name = input("Enter owner name: ")
+        interest_rate = float(input("Enter interest rate: "))
+        minimum_balance = float(input("Enter minimum balance: "))
+
+        return SavingsAccount(
+            account_number,
+            owner_name,
+            interest_rate,
+            minimum_balance
+        )
+
+
+
+
+def main_menu(account):
+    while True:
+        print("\n--- Main Menu ---")
+        print("1. Deposit")
+        print("2. Withdraw")
+        print("3. Transfer")
+        print("4. Check Balance")
+        print("5. View Logs")
+        print("6. Apply Interest")
+        print("7. Exit")
+
+        choice = input("Choose an option: ")
+        if choice == "7":
+            print("Goodbye!")
+            break
+
+account = create_account()
+main_menu(account)
